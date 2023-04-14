9aa7b156cde0a6997bf08d0412282e4332af5bf7
swoo <flycielo@gmail.com>
2023-04-14 17:44:52 +0900

    delete default packages

--- nvim/lua/swoo/plugins/lsp/mason.lua
+++ nvim/lua/swoo/plugins/lsp/mason.lua
@@ -21,9 +21,7 @@ mason.setup()
 
 mason_lspconfig.setup({
 	-- list of servers for mason to install
-	ensure_installed = {
-		"pyright",
-	},
+	ensure_installed = {},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
 })
@@ -31,11 +29,7 @@ mason_lspconfig.setup({
 mason_null_ls.setup({
 	-- list of formatters & linters for mason to install
 	ensure_installed = {
-		"black", -- python formatter,
-		"pylint", -- python linter,
 		"stylua",
-		"prettierd",
-		"jsonlint",
 	},
 	-- auto-install configured formatters & linters (with null-ls)
 	automatic_installation = true,
